9d7168ca25f55846b016af2f4be6098dca3b8de7
MK KNIHT <mk.knight970@gmail.com>
2020-11-28 20:17:52 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('hello javascript');
+
+		let name = document.getElementById('name').value;
+		let email = document.getElementById('email').value;
+		let number = document.getElementById('number').value;
+        let password = document.getElementById('password').value;
